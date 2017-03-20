46c0c013e8a98b6f0801467c446a67fb88c5345e
Shawn Allen <shawnbot@users.noreply.github.com>
2017-03-20 16:20:32 -0700

    Delete 18f-pages.rb

--- _plugins/18f-pages.rb
+++
@@ -1,7 +0,0 @@
-PAGES_BRANCHES = ['master']
-
-if PAGES_BRANCHES.include? ENV['BRANCH']
-    Jekyll::Hooks.register :site, :pre_render do |site|
-        site.config['baseurl'] = '/site/18f/automated-testing-playbook'
-    end
-end
